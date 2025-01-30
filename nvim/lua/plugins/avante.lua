3758737225d0085517c74ac75388fc0f3be1ed92
{{your_name}} <callmenicoletti@gmail.com>
2025-01-29 22:27:25 -0300

    last 29jan

--- nvim/lua/plugins/avante.lua
+++ nvim/lua/plugins/avante.lua
@@ -1,33 +1,10 @@
--- In ~/.config/nvim/lua/plugins/avante.lua
 return {
+
 	{
 		"yetone/avante.nvim",
 		event = "VeryLazy",
 		lazy = false,
-		version = false,
-		build = "make",
-		dependencies = {
-			"stevearc/dressing.nvim",
-			"nvim-lua/plenary.nvim",
-			"MunifTanjim/nui.nvim",
-			"nvim-tree/nvim-web-devicons",
-			"HakonHarnes/img-clip.nvim",
-			event = "VeryLazy",
-			opts = {
-				-- recommended settings
-				default = {
-					embed_image_as_base64 = false,
-					prompt_for_file_name = false,
-					drag_and_drop = {
-						insert_mode = true,
-					},
-					-- required for Windows users
-					use_absolute_path = true,
-				},
-			},
-			"MeanderingProgrammer/render-markdown.nvim",
-			ft = { "markdown", "norg", "rmd", "org", "Avante" },
-		},
+		version = false, -- Set this to "*" to always pull the latest release version, or set it to false to update to the latest code changes.
 		opts = {
 			provider = "openai",
 			auto_suggestions_provider = "openai",
@@ -36,11 +13,12 @@ return {
 				endpoint = "http://192.168.15.128:1234/v1",
 				-- model = "deepseek-r1-distill-llama-8b",
 				-- model = "qwen2.5-7b-instruct-1m",
+				model = "deepseek-r1-distill-qwen-7b",
 				-- model = "qwen2.5-coder-14b-intruct",
-				model = "deepseek-coder-v2-lite-instruct",
+				-- model = "deepseek-coder-v2-lite-instruct",
 				api_key = "OPENAI_API_KEY",
-				temperature = 0.7,
-				max_tokens = 2048,
+				temperature = 0.95,
+				max_tokens = 4096,
 				headers = {}, -- headers vazios para evitar validações desnecessárias
 				["local"] = true,
 			},
@@ -48,8 +26,45 @@ return {
 				auto_suggestions = true,
 				auto_set_highlight_group = true,
 				auto_set_keymaps = true,
+				auto_apply_diff_after_generation = false,
+				support_paste_from_clipboard = true,
+				minimize_diff = true, -- Whether to remove unchanged lines when applying a code block
 			},
+			mappings = {
+				--- @class AvanteConflictMappings
+				diff = {
+					ours = "co",
+					theirs = "ct",
+					all_theirs = "ca",
+					both = "cb",
+					cursor = "cc",
+					next = "]x",
+					prev = "[x",
+				},
+				suggestion = {
+					accept = "<M-l>",
+					next = "<M-]>",
+					prev = "<M-[>",
+					dismiss = "<C-]>",
+				},
+				jump = {
+					next = "]]",
+					prev = "[[",
+				},
+				submit = {
+					normal = "<CR>",
+					insert = "<C-s>",
+				},
+				sidebar = {
+					apply_all = "A",
+					apply_cursor = "a",
+					switch_windows = "<Tab>",
+					reverse_switch_windows = "<S-Tab>",
+				},
+			},
+			hints = { enabled = true },
 			windows = {
+				---@type "right" | "left" | "top" | "bottom"
 				position = "right",
 				wrap = true,
 				width = 30,
@@ -58,6 +73,83 @@ return {
 					align = "center",
 					rounded = true,
 				},
+				input = {
+					prefix = "> ",
+					height = 8, -- Height of the input window in vertical layout
+				},
+				edit = {
+					border = "rounded",
+					start_insert = true, -- Start insert mode when opening the edit window
+				},
+				ask = {
+					floating = false, -- Open the 'AvanteAsk' prompt in a floating window
+					start_insert = true, -- Start insert mode when opening the ask window
+					border = "rounded",
+					---@type "ours" | "theirs"
+					focus_on_apply = "ours", -- which diff to focus after applying
+				},
+			},
+			highlights = {
+				---@type AvanteConflictHighlights
+				diff = {
+					current = "DiffText",
+					incoming = "DiffAdd",
+				},
+			},
+			--- @class AvanteConflictUserConfig
+			diff = {
+				autojump = true,
+				---@type string | fun(): any
+				list_opener = "copen",
+				--- Override the 'timeoutlen' setting while hovering over a diff (see :help timeoutlen).
+				--- Helps to avoid entering operator-pending mode with diff mappings starting with `c`.
+				--- Disable by setting to -1.
+				override_timeoutlen = 500,
+			},
+			suggestion = {
+				debounce = 600,
+				throttle = 600,
+			},
+		},
+		-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+		build = "make",
+		-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+		dependencies = {
+			"nvim-treesitter/nvim-treesitter",
+			"stevearc/dressing.nvim",
+			"nvim-lua/plenary.nvim",
+			"MunifTanjim/nui.nvim",
+			--- The below dependencies are optional,
+			"echasnovski/mini.pick", -- for file_selector provider mini.pick
+			"nvim-telescope/telescope.nvim", -- for file_selector provider telescope
+			"hrsh7th/nvim-cmp", -- autocompletion for avante commands and mentions
+			"ibhagwan/fzf-lua", -- for file_selector provider fzf
+			"nvim-tree/nvim-web-devicons", -- or echasnovski/mini.icons
+			"zbirenbaum/copilot.lua", -- for providers='copilot'
+			{
+				-- support for image pasting
+				"HakonHarnes/img-clip.nvim",
+				event = "VeryLazy",
+				opts = {
+					-- recommended settings
+					default = {
+						embed_image_as_base64 = false,
+						prompt_for_file_name = false,
+						drag_and_drop = {
+							insert_mode = true,
+						},
+						-- required for Windows users
+						use_absolute_path = true,
+					},
+				},
+			},
+			{
+				-- Make sure to set this up properly if you have lazy=true
+				"MeanderingProgrammer/render-markdown.nvim",
+				opts = {
+					file_types = { "markdown", "Avante" },
+				},
+				ft = { "markdown", "Avante" },
 			},
 		},
 	},
